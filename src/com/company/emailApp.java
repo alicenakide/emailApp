076e1ba933ba23a07a621189de7f10468b263b7b
Alice <alice@Alice-2.local>
2018-12-24 20:00:48 +0300

    Initial commit

---
+++ src/com/company/emailApp.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class emailApp {
+
+    public static void main(String[] args) {
+
+        Email em1 = new Email("Alice" , "Norah");
+
+//        em1.changePassword("srt123");
+//        em1.setAlternateEmail("nakide@gmail.com");
+//        em1.setMailboxCapacity(100);
+
+//        System.out.println(em1.getPassword());
+//        System.out.println(em1.getAlternateEmail());
+//        System.out.println(em1.getMailboxCapacity());
+
+        System.out.println(em1.showInfo());
+    }
+
+
+
+}
